88968410679ef792143bd3ae8c7c23bffd0f9a73
Aniket Kadam <anikadamg@gmail.com>
2022-07-14 23:17:05 +0530

    Added webhook verification

--- functions/whatsapp-receive/whatsapp-receive.ts
+++ functions/whatsapp-receive/whatsapp-receive.ts
@@ -1,14 +1,15 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  // const { name = 'stranger' } = event.queryStringParameters
+  const resultOfChallenge = event.queryStringParameters["hub.challenge"]
 
-  const eventBody = JSON.parse(event.body);
-  console.log(`The body of the event is ${eventBody}.`);
+  // const eventBody = JSON.parse(event.body);
+  console.log(`The body of the event is ${event}.`);
   console.log(`The query params are ${event.queryStringParameters}`)
 
   return {
     statusCode: 200,
+    body : resultOfChallenge
     // body: JSON.stringify({
     //   message: `Hello, ${name}!`,
     // }),
