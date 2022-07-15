c81f25d8e250e7e55e458021888db476ab885620
Aniket Kadam <anikadamg@gmail.com>
2022-07-15 15:38:55 +0530

    Printing whatsapp info

--- functions/whatsapp-receive/whatsapp-receive.ts
+++ functions/whatsapp-receive/whatsapp-receive.ts
@@ -1,15 +1,11 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  const resultOfChallenge = event.queryStringParameters["hub.challenge"]
 
-  // const eventBody = JSON.parse(event.body);
-  console.log(`The body of the event is ${event}.`);
-  console.log(`The query params are ${event.queryStringParameters}`)
+  console.log(`The body of the event is ${JSON.stringify(event)}.`);
 
   return {
-    statusCode: 200,
-    body : resultOfChallenge
+    statusCode: 200
     // body: JSON.stringify({
     //   message: `Hello, ${name}!`,
     // }),
